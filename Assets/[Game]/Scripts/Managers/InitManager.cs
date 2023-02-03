e87694dabb9b3ca5bfb985d7ab77ad3ed900e01e
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-02-03 22:00:57 +0300

    Scenes

---
+++ Assets/[Game]/Scripts/Managers/InitManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class InitManager : MonoBehaviour
+{
+    #region Parameters
+    public static InitManager instance = null;
+    #endregion
+    #region MonoBehaviour Methods
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            StartCoroutine(LoadScenesCO());
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+    #region My Methods
+    private IEnumerator LoadScenesCO()
+    {
+        yield return SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        yield return SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+
+    }
+    #endregion
+
+}
