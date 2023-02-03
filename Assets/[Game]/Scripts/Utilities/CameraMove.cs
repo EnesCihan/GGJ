1eb5c8c94bae2da7517824e1b5ce5ea3a0986635
Enes Cihan <enesc1h4n@gmail.com>
2023-02-03 22:36:57 +0300

    CameraMovment

---
+++ Assets/[Game]/Scripts/Utilities/CameraMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    private float horizontalMove;
+    private float verticalMove;
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+        Vector3 moveInput = new Vector3(horizontalMove, 0f, verticalMove);
+        transform.Translate(moveInput * moveSpeed * Time.deltaTime);
+    }
+}
