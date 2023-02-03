00a67ad57de4e8e18fd6976b699f1316cb4bf81c
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-02-03 22:38:19 +0300

    Update

--- Assets/[Game]/Scripts/SceneInitializer.cs
+++ Assets/[Game]/Scripts/SceneInitializer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Test : MonoBehaviour
+public class SceneInitializer : MonoBehaviour
 {
     private void Start()
     {
