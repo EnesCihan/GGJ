365dc40c4f909ae1663f77e06882be6a83d061c6
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-02-03 22:34:36 +0300

    Update

---
+++ Assets/[Game]/Scripts/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        if (SceneManager.sceneCount > 1)
+        {
+            Destroy(gameObject);
+        }else
+            StartCoroutine(LoadScenesCO());
+    }
+    private IEnumerator LoadScenesCO()
+    {
+        yield return SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
+}
