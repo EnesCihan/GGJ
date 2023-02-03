8b8a39dfed828bfcbc9bc9da6c178e771af74c49
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-02-04 00:02:16 +0300

    InputManager

--- Assets/[Game]/Scripts/Managers/EventManager.cs
+++ Assets/[Game]/Scripts/Managers/EventManager.cs
@@ -3,8 +3,10 @@ using UnityEngine.Events;
 public class EventManager : MonoBehaviour
 {
     public static UnityEvent OnLevelStart = new UnityEvent();
-    public static UnityEvent OnOpenMenu = new UnityEvent();
-    public static UnityEvent OnCloseMenu = new UnityEvent();
+    public static UnityEvent CloseSettingsMenu = new UnityEvent();
+    public static UnityEvent OpenSettingsMenu = new UnityEvent();
+    public static UnityEvent OnMenu = new UnityEvent();
+    public static UnityEvent OnGameStart = new UnityEvent();
     public static UnityEvent OnWin = new UnityEvent();
     public static UnityEvent OnLoose = new UnityEvent();
 }
--- Assets/[Game]/Scripts/Managers/InputManager.cs
+++ Assets/[Game]/Scripts/Managers/InputManager.cs
@@ -6,46 +6,76 @@ public class InputManager : MonoBehaviour
     #region Params
     bool onGame;
     bool onMenu;
+    bool onOptions;
     #endregion
 
     #region MyMethods
-    private void MenuOpen()
+    private void SettingsMenu()
     {
         onGame = false;
+        onMenu = false;
+        onOptions = true;
+    }
+    private void OnMenu()
+    {
+        onGame = false;
+        onOptions = false;
         onMenu = true;
     }
     private void OnGameOpen()
     {
-        onGame = true;
         onMenu = false;
+        onOptions = false;
+        onGame = true;
+    }
+    private void OnStartGame()
+    {
+        onMenu = false;
+        onOptions = false;
+        onGame = false;
     }
     #endregion
     #region MonoBehaviourFunctions
-    void Awake()
-    {
-         
-    }
+
     private void OnEnable()
     {
         EventManager.OnLevelStart.AddListener(OnGameOpen);
-        EventManager.OnOpenMenu.AddListener(MenuOpen);
-        EventManager.OnCloseMenu.AddListener(OnGameOpen);
+        EventManager.OnMenu.AddListener(OnMenu);
+        EventManager.OnGameStart.AddListener(OnStartGame);
+        EventManager.OpenSettingsMenu.AddListener(SettingsMenu);
+        EventManager.CloseSettingsMenu.AddListener(OnGameOpen);
     }
     private void OnDisable()
     {
         EventManager.OnLevelStart.RemoveListener(OnGameOpen);
-        EventManager.OnOpenMenu.RemoveListener(MenuOpen);
-        EventManager.OnCloseMenu.RemoveListener(OnGameOpen);
+        EventManager.OnMenu.RemoveListener(OnMenu);
+        EventManager.OnGameStart.RemoveListener(OnStartGame);
+        EventManager.OpenSettingsMenu.RemoveListener(SettingsMenu);
+        EventManager.CloseSettingsMenu.RemoveListener(OnGameOpen);
 
     }
 
     void Update()
     {
-        if (!onGame && !onMenu)
+        if (!onGame && !onMenu && !onOptions)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 EventManager.OnLevelStart.Invoke();
+            }
+        }
+        if(onGame)
+        {
+            if(Input.GetButton("ESC"))
+            {
+                EventManager.OpenSettingsMenu.Invoke();
+            }
+        }
+        if (onOptions)
+        {
+            if (Input.GetButton("ESC"))
+            {
+                EventManager.CloseSettingsMenu.Invoke();
             }
         }
     }
