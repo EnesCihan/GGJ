96e2751a277cf78279e0ec47bdc7660bfb877879
Enes Cihan <enesc1h4n@gmail.com>
2023-02-04 15:51:56 +0300

    Camera ve Enverimont

--- Assets/[Game]/Scripts/Utilities/CameraMove.cs
+++ Assets/[Game]/Scripts/Utilities/CameraMove.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraMove : MonoBehaviour
 {
@@ -8,11 +7,18 @@ public class CameraMove : MonoBehaviour
     private float moveSpeed;
     private float horizontalMove;
     private float verticalMove;
+    public CinemachineVirtualCamera cmv;
+    private float currentZoom;
+    private const float MAX_ZOOM = 20f;
 
-
+    private void Start()
+    {
+        currentZoom = cmv.m_Lens.FieldOfView;
+    }
     void Update()
     {
         Move();
+        Zoom();
     }
 
     private void Move()
@@ -21,5 +27,20 @@ public class CameraMove : MonoBehaviour
         verticalMove = Input.GetAxisRaw("Vertical");
         Vector3 moveInput = new Vector3(horizontalMove, 0f, verticalMove);
         transform.Translate(moveInput * moveSpeed * Time.deltaTime);
+    }
+
+    private void Zoom()
+    {
+        Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            //cmv.m_Lens.FieldOfView = Mathf.Lerp(currentZoom, MAX_ZOOM, Time.deltaTime * 20f);
+            currentZoom = 20f;
+            cmv.m_Lens.FieldOfView = currentZoom;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            cmv.m_Lens.FieldOfView = Mathf.Lerp(currentZoom, 60f, Time.deltaTime * 20f);
+        }
     }
 }
